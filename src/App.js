6788b7de5df282d5f86b14d8d752bfb4427604f8
Vitaliy <klaot@yandex.ru>
2022-07-21 23:34:32 +0300

    добавил блок Our Services

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import AboutUs from './components/AboutUs.js';
 import Header from './components/Header.js';
 import Navigation from './components/Navigation.js';
+import OurServices from './components/OurServices.js';
+import WhatWeOffer from './components/WhatWeOffer.js';
 import './styles/styles.scss';
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
         <Navigation />
         <Header />
         <AboutUs/>
+        <WhatWeOffer/>
+        <OurServices/>
     </div>
   );
 }
