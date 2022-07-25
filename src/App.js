99675a3f057b9552ed8fa0a6cdb07bf478d6b71a
Vitaliy <klaot@yandex.ru>
2022-07-25 22:39:37 +0300

    добавил немного стилей и блок галерея

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import AboutUs from './components/AboutUs.js';
 import Header from './components/Header.js';
 import Navigation from './components/Navigation.js';
 import OurServices from './components/OurServices.js';
+import TheGallery from './components/TheGallery.js';
 import WhatWeOffer from './components/WhatWeOffer.js';
 import WhyChooseUs from './components/WhyChooseUs.js';
 import './styles/styles.scss';
@@ -16,6 +17,7 @@ function App() {
         <WhatWeOffer/>
         <OurServices/>
         <WhyChooseUs/>
+        <TheGallery/>
     </div>
   );
 }
